a8fd76aab6503b690e0676e5ff512ab4ad519abc
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-09-09 15:16:19 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Темергалеев Никита Алексеевич, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Темергалеев Никита, ФБИ-11, 3 курс, 2023
+        </footer>
+    </body>
+</html> 
+""" 
