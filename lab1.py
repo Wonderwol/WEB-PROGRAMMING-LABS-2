430649835531eb15673b842bf2646141d32f5f44
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-11-07 02:03:20 +0700

    Заготовка к 4-ой лабораторной работе

--- lab1.py
+++ lab1.py
@@ -32,6 +32,9 @@ def menu():
             <li>
                 <a href="/lab3" target="_blank">Третья лабораторная работа</a>
             </li>
+            <li>
+                <a href="/lab4" target="_blank">Четвертая лабораторная работа</a>
+            </li>
         </ol>
 
         <footer>
