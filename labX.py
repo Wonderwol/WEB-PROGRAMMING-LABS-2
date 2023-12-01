d3ad70b4220b55e26c1f35729dff482cad020fc9
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-12-02 03:34:40 +0700

    Реализация регистрации и входа

--- labX.py
+++ labX.py
@@ -4,7 +4,6 @@ import psycopg2
 
 
 lab5 = Blueprint("lab5", __name__)
-global_result = []
 
 
 def dbConnect():
@@ -26,33 +25,35 @@ def dbClose(cursor, conn):
 
 @lab5.route("/lab5/")
 def main():
-
+    # Прописываем параметры для подключения к БД
     conn = dbConnect()
+    # Получаем курсор. С помощью него мы можем выполнять SQL-запросы
     cur = conn.cursor()
 
+    # Пишем запрос, который psycopg2 должен выполнить
     cur.execute("SELECT * FROM users;")
 
+    # fetchall - получить все строки, которые получились в результате
+    # выполнения SQL- запроса в execute
+    # Сохраняем эти строки в переменную result
+    # Используем global, чтобы передавать result в другие функции
     result = cur.fetchall()
-    global global_result
-    global_result = result
+    session['result'] = result
 
-    username = session.get('username', 'Anon')
+    # Закрываем соединение с БД
     dbClose(cur, conn)
-    if not username:
-        visibleUser = 'Anon'
-    else:
-        visibleUser = username
 
+    visibleUser = 'Anon'
 
     return render_template('lab5.html', username=visibleUser, result=result)
 
 
 @lab5.route('/lab5/users')
 def users():
-    global global_result
-
-    len_res = len(global_result)
-    return render_template('users.html', result=global_result, len_res=len_res)
+    # Получаем значение result через session
+    result = session.get('result', [])
+    len_res = len(result)
+    return render_template('users.html', result=result, len_res=len_res)
 
 
 @lab5.route('/lab5/register', methods=["GET", "POST"])
@@ -62,34 +63,56 @@ def registerPage():
     if request.method == 'GET':
         return render_template("register.html", errors=errors)
 
+    # Если мы попали сюда, значит это метод POST,
+    # так как GET мы уже обработали и сделали return.
+    # После return функция немедленно завершается
     username = request.form.get("username")
     password = request.form.get("password")
 
+    # Проверяем username и password на пустоту
+    # Если любой из них пустой, то добавляем ошибку
+    # и рендерим шаблом
     if not (username or password):
         errors.append("Пожалуйста, заполните все поля")
         print(errors)
         return render_template("register.html", errors=errors)
 
+    # Получаем пароль от пользователя, хэшируем его
     hashPassword = generate_password_hash(password)
 
+    # Если мы попали сюда, значит username и password заполнены
+    # Подключаемся к БД
     conn = dbConnect()
-    cur = conn.cursor()
+    cur = conn.cursor()  # Исправить фигню с connections
 
+    # Проверяем наличие клиента в базе данных
+    # У нас может быть 2 пользователя с одинаковыми логинами
+
+    # WARNING: мы используем f-строки, что не рекомендуется делать
+    # позже мы разберемся с Вами почему не стоит так делать
     cur.execute(f"SELECT username FROM users WHERE username = '{ username }';")
 
-    resultСur = cur.fetchone()
-
-    if resultСur != None:
+    # fetchone, в отличие от fetchall, получает только одну строку
+    # мы задали свойство UNIQUE для пользователя, значит
+    # больше одной строки мы не можем получить
+    # Только один пользователь с таким именем может быть в БД
+    if cur.fetchone() is not None:
         errors.append('Пользователь с данным именем уже существует')
 
-        dbClose(cur, conn)
+        conn.close()
+        cur.close()
 
-        return render_template('register.html', errors=errors, resultСur=resultСur)
+        return render_template('register.html', errors=errors)
 
+    # Если мы попали сюда, то значит в cur.fetchall ни одной строки
+    # значит пользователь с таким же логином не существует
+    # Сохраняем пароль в виде хэша в БД
     cur.execute(f"INSERT INTO users (username, password) VALUES ('{username}', '{hashPassword}');")
 
-    conn.commit()
-    dbClose(cur, conn)
+    # Делаем commit - т. е. фиксируем изменения
+    conn.commit
+    conn.close()
+    cur.close()
 
     return redirect("/lab5/login")
 
