3086d0e38062bb9e7858d896126ebcebaedca2f0
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-12-17 18:00:24 +0700

    Редактирование выбранного курса по REST

--- lab8.py
+++ lab8.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, abort
+from flask import Blueprint, render_template, request
 
 
 lab8 = Blueprint('lab8', __name__)
@@ -7,6 +7,7 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def main():
     return render_template('lab8/index.html')
+
 
 courses = [
     {"name": "c++", "videos": 3, "price": 3000},
@@ -35,3 +36,13 @@ def del_course(course_num):
         return '', 204
     else:
         return '', 404
+
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['PUT'])
+def put_course(course_num):
+    course = request.get_json()
+    courses[course_num] = course
+    if course_num in range(0, len(courses)):
+        return courses[course_num]
+    else:
+        return "", 404
