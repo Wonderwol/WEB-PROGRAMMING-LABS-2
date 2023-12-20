6fdc7ebd722b9ea8df5bffe942d904067d42e76a
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-12-20 15:06:50 +0700

    Обработчик ошибки 494

--- lab9.py
+++ lab9.py
@@ -2,6 +2,11 @@ from flask import Blueprint, render_template, request, jsonify
 
 lab9 = Blueprint('lab9', __name__)
 
+
 @lab9.route('/lab9/')
 def main():
     return render_template('lab9/index.html')
+
+@lab9.app_errorhandler(404)
+def not_found(err):
+    return render_template('error404.html')
