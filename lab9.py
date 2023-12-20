0e7b97c7f56034c08ba4dd6cf780c706d8aa8457
Темергалеев Никита <mr.wonderwol@mail.ru>
2023-12-20 15:18:55 +0700

    Обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, abort
 
 lab9 = Blueprint('lab9', __name__)
 
@@ -7,6 +7,18 @@ lab9 = Blueprint('lab9', __name__)
 def main():
     return render_template('lab9/index.html')
 
+
 @lab9.app_errorhandler(404)
 def not_found(err):
     return render_template('error404.html')
+
+
+@lab9.app_errorhandler(500)
+def error500(err):
+    return render_template('error500.html')
+
+
+@lab9.route('/lab9/500')
+def error():
+    result = 1 / 0
+    return result
